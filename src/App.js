afd6b998a9fc609920a7bcc0e517b5ad435fe401
developedbyssony <n_uck@naver.com>
2022-06-29 14:27:51 +0900

    20220629

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import styles from './App.module.css';
 import VideoList from './components/video_list/video_list';
+import Header from './components/search_header/search_header'
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+     youtube
+     .search(query)
+     .then(videos => setVideos(videos));
+  };
 
   useEffect(()=> {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBS2YZE8ZLJd6Oo18NKJS3Am_QwV7EYy8s", 
-          requestOptions
-      ).then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-  },[]); // 컴포넌트가 마운트될 때 1회 콜백 호출
-
-  useEffect(()=> {
-    console.log('setVideos');
-  },[videos]); // video가 바뀔때마다 콜백 호출
-
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
+  },[]);
+  
   return (
-    <>
-        <VideoList videos={videos} />
-    </>
+    <div className={styles.app}>
+      <Header onSearch={search}/>
+      <VideoList videos={videos} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Youtube from './components/service/youtube';
+
+const youtubeInjection = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+// 유닛 테스트시에는 네트워크 통신을 하는 객체가 아닌
+// 정해진 데이터를 리턴하는 객체를 만들어 주입해주면 된다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtubeInjection}/>
   </React.StrictMode>
 );
