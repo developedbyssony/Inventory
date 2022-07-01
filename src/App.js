53b3538b3edf598f61fb16da9f7cba9fbc791ea8
developedbyssony <n_uck@naver.com>
2022-07-01 13:21:01 +0900

    20220701

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
 import styles from './App.module.css';
 import VideoList from './components/video_list/video_list';
+import VideoDetail from './components/video_detail/video_detail';
 import Header from './components/search_header/search_header'
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
+
   const search = query => {
      youtube
      .search(query)
@@ -20,7 +27,16 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <Header onSearch={search}/>
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        { selectedVideo && (
+        <div className={styles.detail}>
+        <VideoDetail video={selectedVideo} />
+        </div>
+         )}
+        <div className={styles.list}>
+        <VideoList videos={videos} onVideoClick={selectVideo} display={ selectedVideo ? 'list' : 'grid' }/>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const videoItem = ({video: { snippet }}) =>
-        <li className={styles.container}>
+const videoItem = ({video, video: { snippet }, onVideoClick, display }) => {
+    const displayType = display === 'list' ? styles.list : styles.grid;
+    
+    return (
+        <li className={`${styles.container} ${displayType}`} onClick={() => onVideoClick(video)}>
             <div className={styles.video}>
             <img
               className={styles.thumbnail}
               src={snippet.thumbnails.medium.url} 
-              alt="thumbnail" 
+              alt="video thumbnail" 
               />
         <div className={styles.metadata}>
             <p className={styles.title}>{snippet.title}</p>
@@ -15,5 +18,7 @@ const videoItem = ({video: { snippet }}) =>
         </div>
         </div>
         </li>
+    );
+};
 
 export default videoItem
