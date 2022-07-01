72365debc032b366343082ddb39cd908a71d0bbe
developedbyssony <n_uck@naver.com>
2022-07-01 09:09:35 +0900

    🚧

--- src/components/service/youtube.js
+++ src/components/service/youtube.js
@@ -11,7 +11,7 @@ class Youtube {
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
         this.getrequestOptions
         );
-        const result = response.json();
+        const result = await response.json();
         return result.items;
         } 
 
@@ -19,7 +19,7 @@ class Youtube {
         const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`, 
         this.getrequestOptions
         );
-        const result = response.json();
+        const result = await response.json();
         return result.items.map(item => ({...item,id:item.id.videoId}));
     }
 }
